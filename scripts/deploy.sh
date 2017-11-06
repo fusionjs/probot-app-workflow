ff64675b515dac8a073d37f8c71a310e48ca4139
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-11-06 11:51:37 -0800

    Pin now canary version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-yarn global add now@canary now-replace
+yarn global add now@9.0.0-canary.6 now-replace
 APP_URL=$(now -t $NOW_TOKEN --public -e PRIVATE_KEY=@private-key -e APP_ID=@app-id -e WEBHOOK_SECRET=@webhook-secret -e NODE_ENV="production")
 now scale $APP_URL 1 -t $NOW_TOKEN
 now-replace fusion-probot $APP_URL -t $NOW_TOKEN
