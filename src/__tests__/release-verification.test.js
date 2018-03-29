d55346269c421c7137671b1ea2998eae8067ee5d
Kevin Grandon <kevingrandon@yahoo.com>
2018-03-29 10:58:33 +0900

    Add testing for release-verification

---
+++ src/__tests__/release-verification.test.js
@@ -0,0 +1,63 @@
+import {createRobot} from 'probot';
+import app from '../release-verification';
+
+import nonReleasePayload from './fixtures/non-release-payload.json';
+import releasePayload from './fixtures/release-payload.json';
+
+jest.mock('child_process', () => {
+  return {
+    exec: (command, callback) =>
+      callback(null, '{"web_url": "fusion-verification-url"}'),
+  };
+});
+
+describe('release-verification', () => {
+  let robot;
+  let github;
+
+  beforeEach(() => {
+    robot = createRobot();
+    app(robot);
+    github = {
+      issues: {
+        createComment: jest.fn(),
+      },
+      repos: {
+        createStatus: jest.fn().mockReturnValue(Promise.resolve(true)),
+      },
+    };
+    // Passes the mocked out GitHub API into out robot instance
+    robot.auth = () => Promise.resolve(github);
+  });
+
+  it('non-release tag applied', async () => {
+    await robot.receive({
+      event: 'pull_request',
+      payload: nonReleasePayload,
+    });
+    // Should immediately set success
+    const statusCalls = github.repos.createStatus.mock.calls;
+    expect(github.repos.createStatus).toHaveBeenCalled();
+    expect(github.issues.createComment.mock.calls.length).toBe(0);
+    expect(statusCalls.length).toBe(2);
+    expect(statusCalls[0][0].state).toBe('pending');
+    expect(statusCalls[1][0].state).toBe('success');
+  });
+
+  it('release tag applied', async () => {
+    await robot.receive({
+      event: 'pull_request',
+      payload: releasePayload,
+    });
+    // Should immediately set success (for now), and comment.
+    const statusCalls = github.repos.createStatus.mock.calls;
+    expect(github.repos.createStatus).toHaveBeenCalled();
+    expect(github.issues.createComment.mock.calls.length).toBe(1);
+    expect(github.issues.createComment.mock.calls[0][0].body).toContain(
+      'fusion-verification-url'
+    );
+    expect(statusCalls.length).toBe(2);
+    expect(statusCalls[0][0].state).toBe('pending');
+    expect(statusCalls[1][0].state).toBe('success');
+  });
+});
