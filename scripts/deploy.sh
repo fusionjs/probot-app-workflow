60808942f1499bf0409d672810bf01c29a5ba213
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-04-09 11:39:10 -0700

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-yarn global add now@9.0.1 now-replace
+yarn global add now@11.0.6
 APP_URL=$(now --npm -t $NOW_TOKEN --public -e PRIVATE_KEY=@private-key -e APP_ID=@app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@webhook-secret -e NODE_ENV="production")
 now scale $APP_URL 1 -t $NOW_TOKEN
-now-replace fusion-probot $APP_URL -t $NOW_TOKEN
+now alias set $APP_URL fusion-probot -t $NOW_TOKEN
+now rm fusion-probot --safe
