dee32447c657e7b4326d4d32993ea5de893bf8ca
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-04-09 12:22:47 -0700

    Supress stdout

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 yarn global add now@11.0.6
 APP_URL=$(now --npm -t $NOW_TOKEN --public -e PRIVATE_KEY=@private-key -e APP_ID=@app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@webhook-secret -e NODE_ENV="production")
