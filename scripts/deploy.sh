b1eee8fa009e0b74e22c51c6b0856e2b26694862
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-04-09 11:52:53 -0700

    Only deploy to one datacenter

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ set -ex
 
 yarn global add now@11.0.6
 APP_URL=$(now --npm -t $NOW_TOKEN --public -e PRIVATE_KEY=@private-key -e APP_ID=@app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@webhook-secret -e NODE_ENV="production")
-now scale $APP_URL 1 --token=$NOW_TOKEN
+now scale $APP_URL sfo 1 --token=$NOW_TOKEN
 now alias set $APP_URL fusion-probot -t $NOW_TOKEN
 now rm fusion-probot --safe -t $NOW_TOKEN
