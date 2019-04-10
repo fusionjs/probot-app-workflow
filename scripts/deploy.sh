31008b3355c2827ae530c5bde2f0d6bb4a13c178
Chris Deacy <deacy@uber.com>
2019-04-10 12:13:11 -0700

    Update deploy script, Use new now token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 yarn global add now@11.0.6
-APP_URL=$(now --npm -n temporary-probot-app -t $NOW_TOKEN --public -e PRIVATE_KEY=@github-app-private-key -e APP_ID=@github-app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@github-app-webhook-secret -e NODE_ENV="production")
-now scale $APP_URL sfo 1 --token=$NOW_TOKEN
-now alias set $APP_URL fusion-monorepo-probot -t $NOW_TOKEN
-now rm probot-app-workflow --safe -t $NOW_TOKEN -y
+APP_URL=$(now --npm --name=fusion-monorepo-bot -t $NOW_MONOREPO_TOKEN --public -e PRIVATE_KEY=@github-app-private-key -e APP_ID=@github-app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@github-app-webhook-secret -e NODE_ENV="production")
+now scale $APP_URL sfo 1 --token=$NOW_MONOREPO_TOKEN
+now alias set $APP_URL fusion-monorepo-bot -t $NOW_MONOREPO_TOKEN
+now rm fusion-monorepo-bot --safe -t $NOW_MONOREPO_TOKEN -y
