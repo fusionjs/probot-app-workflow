cbe3daed6030427f0c987d6fab1aac311c4a993c
Chris Deacy <deacy@uber.com>
2019-04-10 14:24:16 -0700

    Add now team

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+team=$NOW_TEAM
+token=$NOW_MONOREPO_TOKEN
+
 yarn global add now@11.0.6
-APP_URL=$(now --npm --name=fusion-monorepo-bot -t $NOW_MONOREPO_TOKEN --public -e PRIVATE_KEY=@github-app-private-key -e APP_ID=@github-app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@github-app-webhook-secret -e NODE_ENV="production")
-now scale $APP_URL sfo 1 --token=$NOW_MONOREPO_TOKEN
-now alias set $APP_URL fusion-monorepo-bot -t $NOW_MONOREPO_TOKEN
-now rm fusion-monorepo-bot --safe -t $NOW_MONOREPO_TOKEN -y
+APP_URL=$(now --npm --name=fusion-monorepo-bot -T $team -t $token --public -e PRIVATE_KEY=@github-app-private-key -e APP_ID=@github-app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@github-app-webhook-secret -e NODE_ENV="production")
+now scale $APP_URL sfo 1 -T $team --token=$token
+now alias set $APP_URL fusion-monorepo-bot -T $team -t $token
+now rm fusion-monorepo-bot --safe -T $team -t $token -y
