b55bbec7bbd3aae369208cd84ea5d9a11f1493e0
Chris Deacy <chrisdothtml@gmail.com>
2019-04-10 10:02:39 -0700

    Add config for replacement bot
    https://github.com/fusionjs/probot-app-workflow/pull/139
    
    Co-authored-by: Chris Deacy <deacy@uber.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 yarn global add now@11.0.6
-APP_URL=$(now --npm -t $NOW_TOKEN --public -e PRIVATE_KEY=@private-key -e APP_ID=@app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@webhook-secret -e NODE_ENV="production")
+APP_URL=$(now --npm -n temporary-probot-app -t $NOW_TOKEN --public -e PRIVATE_KEY=@github-app-private-key -e APP_ID=@github-app-id -e BUILDKITE_TOKEN=@buildkite-token -e WEBHOOK_SECRET=@github-app-webhook-secret -e NODE_ENV="production")
 now scale $APP_URL sfo 1 --token=$NOW_TOKEN
-now alias set $APP_URL fusion-probot -t $NOW_TOKEN
+now alias set $APP_URL fusion-monorepo-probot -t $NOW_TOKEN
 now rm probot-app-workflow --safe -t $NOW_TOKEN -y
