a92884a37c943e0dcc40eaa6b4257712ecf6cda4
Chris Deacy <deacy@uber.com>
2019-05-13 12:26:27 -0700

    Upgrade probot-app-monorepo-sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ yarn global add now@11.0.6
 # multiple orgs, so we need a separate deployment for each org
 for org in "${orgs[@]}"; do
   app_name="$org-bot"
-  app_url=$(now --npm --name=$app_name -T $team -t $token --public -e PRIVATE_KEY="@probot-$org-private-key" -e APP_ID="@probot-$org-app-id" -e WEBHOOK_SECRET="@probot-$org-webhook-secret" -e BUILDKITE_TOKEN=@buildkite-token -e REPO_RELATIONSHIPS="$REPO_RELATIONSHIPS" -e NODE_ENV="production")
+  app_url=$(now --npm --name=$app_name -T $team -t $token --public -e PRIVATE_KEY="@probot-$org-private-key" -e APP_ID="@probot-$org-app-id" -e WEBHOOK_SECRET="@probot-$org-webhook-secret" -e BUILDKITE_TOKEN=@buildkite-token -e GH_TOKEN="$GH_TOKEN" -e REPO_RELATIONSHIPS="$REPO_RELATIONSHIPS" -e NODE_ENV="production")
   now scale $app_url sfo 1 -T $team --token=$token
   now alias set $app_url $app_name -T $team -t $token
   now rm $app_name --safe -T $team -t $token -y || true
