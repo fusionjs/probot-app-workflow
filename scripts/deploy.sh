4433d52c18c8f49179254d38383c6ae34f1993c3
Chris Deacy <deacy@uber.com>
2019-06-05 14:24:07 -0700

    Fix spacing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ org="fusionjs"
 app_name="$org-bot"
 
 yarn global add now@11.0.6
-app_url=$(now --npm --name=$app_name -T $team -t $token --public -e PRIVATE_KEY="@probot-$org-private-key" -e APP_ID="@probot-$org-app-id" -e WEBHOOK_SECRET="@probot-$org-webhook-secret" -e BUILDKITE_TOKEN=@buildkite-token -e GH_TOKEN="$GH_TOKEN" -e REPO_RELATIONSHIPS="$REPO_RELATIONSHIPS"-e PROBOT_REPO_WHITELIST="$PROBOT_REPO_WHITELIST"  -e NODE_ENV="production")
+app_url=$(now --npm --name=$app_name -T $team -t $token --public -e PRIVATE_KEY="@probot-$org-private-key" -e APP_ID="@probot-$org-app-id" -e WEBHOOK_SECRET="@probot-$org-webhook-secret" -e BUILDKITE_TOKEN=@buildkite-token -e GH_TOKEN="$GH_TOKEN" -e REPO_RELATIONSHIPS="$REPO_RELATIONSHIPS" -e PROBOT_REPO_WHITELIST="$PROBOT_REPO_WHITELIST" -e NODE_ENV="production")
 now scale $app_url sfo 1 -T $team --token=$token
 now alias set $app_url $app_name -T $team -t $token
 now rm $app_name --safe -T $team -t $token -y || true
